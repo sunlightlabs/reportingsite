233fd2d72e6e3a080ed9d36ed4d26503a4130aa1
Ali Felski <afelski@sunlightfoundation.com>
2009-12-10 09:52:38 -0500

    adding images for the footer and tweeting small things on the home page

---
+++ local_settings.py
@@ -0,0 +1,12 @@
+DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+DATABASE_NAME = 'reporting.sql'             # Or path to database file if using sqlite3.
+DATABASE_USER = ''             # Not used with sqlite3.
+DATABASE_PASSWORD = ''         # Not used with sqlite3.
+DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
+DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
+
+MEDIA_ROOT = '/Users/alifelski/Projects/reportingsite/media/'
+
+TEMPLATE_DIRS = (
+    '/Users/alifelski/Projects/reportingsite/templates'
+)
