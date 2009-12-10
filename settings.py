3a331b952e629c5226d15c9ee78935de848880d3
Luke <luke@luke-desktop.(none)>
2009-12-10 10:31:56 -0500

    settings.py path

--- settings.py
+++ settings.py
@@ -11,7 +11,7 @@ ADMINS = (
 MANAGERS = ADMINS
 
 DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = '/home/luke/reportingsite/reporting.sql'             # Or path to database file if using sqlite3.
+DATABASE_NAME = '/home/reportingsite/reporting.sql'             # Or path to database file if using sqlite3.
 DATABASE_USER = ''             # Not used with sqlite3.
 DATABASE_PASSWORD = ''         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
@@ -36,7 +36,7 @@ USE_I18N = True
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = '/home/luke/reportingsite/media/'
+MEDIA_ROOT = '/home/reportingsite/media/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
@@ -72,7 +72,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/home/luke/reportingsite/templates'
+    '/home/reportingsite/templates'
 )
 
 INSTALLED_APPS = (
