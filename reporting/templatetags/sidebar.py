5f49b2eaa95879488c3961e43127cddb63670be2
Luke <luke@luke-desktop.(none)>
2009-12-18 17:14:41 -0500

    removed unused templates, other changes

---
+++ reporting/templatetags/sidebar.py
@@ -0,0 +1,71 @@
+from django.template import Library, Node
+import re
+from reporting.models import *
+from feedinator import Feed, FeedEntry
+
+register = Library()
+
+
+class OutsideArticles(Node):
+    def render(self, context):
+        context['entries'] = FeedEntry.objects.filter(feed__codename__startswith='News-')[:8]
+        return ''
+    
+def get_outside_articles(parser, token):
+    return OutsideArticles()
+get_outside_articles = register.tag(get_outside_articles)
+
+
+class OurPosts(Node):
+    def __init__(self, exclude):
+        self.exclude = exclude
+    def render(self, context):
+        if self.exclude:
+            context['ourposts'] = Post.objects.published().exclude(pk=context[self.exclude].pk)[:6]
+        else:
+            context['ourposts'] = Post.objects.published()[:6]
+        return ''
+    
+def get_ourposts(parser, token):
+    bits = token.contents.split()
+    exclude = None
+    if len(bits)>1:
+        exclude = bits[1]
+    return OurPosts(exclude)
+get_ourposts = register.tag(get_ourposts)
+
+
+
+class Calendar(Node):
+    def render(self, context):    
+        import time, datetime
+        from operator import itemgetter 
+        cal = FeedEntry.objects.filter(feed__codename__startswith='Calendar-')  
+        cl = []
+        today = datetime.datetime.now().date()
+        for e in cal:
+            d = e.summary[10:22]
+            t = time.strptime(d, "%b %d, %Y")
+            t = datetime.date.fromtimestamp(time.mktime(t))
+            if t>=today:
+                cl.append({'date': t, 'summary': e.title })
+        cl = sorted(cl, key=itemgetter('date'))        
+        context['events'] = cl
+        return ''
+
+def get_calendar(parser, token):
+    return Calendar()
+get_calendar = register.tag(get_calendar)
+
+    
+
+class ResourceFeed(Node):
+    def render(self, context):
+        context['entries'] = FeedEntry.objects.filter(feed__codename__startswith='Resource-')[:10]
+        return ''
+    
+def get_resources(parser, token):
+    return ResourceFeed()
+get_resources = register.tag(get_resources)
+
+
