10cc280c4e334593ac867f7055fb5c8821e49222
Luke <luke@luke-desktop.(none)>
2010-02-19 12:30:36 -0500

    adding files

--- context_processors.py
+++ context_processors.py
@@ -9,3 +9,23 @@ def latest_by_site(request):
 
     return {'FLIT': FLIT, 'SS': SS, 'PT': PT, 'SLRG': SLRG }
 
+
+
+from millions.models import *
+ 
+def filters(request):
+    baseq = Record.objects.filter(version_flag='F').exclude(status='x')
+    recipient_state = baseq.values_list('recipient_state', flat=True).distinct().order_by('recipient_state')
+    awarding_agency_name = baseq.values_list('awarding_agency_name', flat=True).distinct().order_by('awarding_agency_name')
+    recipient_namee = baseq.values_list('recipient_namee', flat=True).distinct().order_by('recipient_namee')
+    pop_state_cd = baseq.values_list('pop_state_cd', flat=True).distinct().order_by('pop_state_cd')
+    infrastructure_state_cd = baseq.values_list('infrastructure_state_cd', flat=True).distinct().order_by('infrastructure_state_cd')
+    project_activity_desc = baseq.values_list('project_activity_desc', flat=True).distinct().order_by('project_activity_desc')
+    award_type = ['', 'Contract', 'Grant', 'Loan']
+
+    filters = {'recipient_state': recipient_state, 'awarding_agency_name': awarding_agency_name, 'pop_state_cd': pop_state_cd, 'infrastructure_state_cd': infrastructure_state_cd, 'award_type': award_type, 'project_activity_desc': project_activity_desc, 'recipient_namee': recipient_namee }
+
+    return { 'filters': filters }
+
+
+
