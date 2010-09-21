ad3a81da907e88b87a427ffbf1f190a88f36a2d2
Aaron Bycoffe <bycoffe@gmail.com>
2010-09-21 15:57:37 -0400

    Misc template changes

--- buckley/templatetags/buckley.py
+++ buckley/templatetags/buckley.py
@@ -6,3 +6,13 @@ register = template.Library()
 @register.filter
 def committee_candidate_money(committee, candidate):
     return committee.money_spent_on_candidate(candidate)
+
+@register.filter
+def absolute_value(n):
+    try:
+        n = int(n)
+    except ValueError:
+        return n
+    if n < 0:
+        return str(n * -1)
+    return n
