1c384286bd49bfbb34972eb5bb76ebe35e2bcb4a
Aaron Bycoffe <bycoffe@gmail.com>
2010-09-22 18:20:03 -0400

    Fix numbers for expenditures in each race

--- buckley/templatetags/buckley.py
+++ buckley/templatetags/buckley.py
@@ -24,3 +24,28 @@ def absolute_value(n):
     if n < 0:
         return str(n * -1)
     return n
+
+@register.filter
+def total_support_by_election_type(candidate, election_type):
+    if not election_type:
+        return candidate.total_supporting()
+    if election_type == 'primary':
+        return candidate.total_by_election_type('P', 'S')
+    if election_type == 'general':
+        return candidate.total_by_election_type('G', 'S')
+    else:
+        return candidate.total_by_election_type('O', 'S')
+
+
+@register.filter
+def total_opposition_by_election_type(candidate, election_type):
+    if not election_type:
+        return candidate.total_opposing()
+    if election_type == 'primary':
+        return candidate.total_by_election_type('P', 'O')
+    if election_type == 'general':
+        return candidate.total_by_election_type('G', 'O')
+    else:
+        return candidate.total_by_election_type('O', 'O')
+
+
