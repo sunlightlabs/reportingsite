201e22875c081e9326e6ed32cc925031fe083c11
Aaron Bycoffe <bycoffe@gmail.com>
2010-10-25 12:07:43 -0400

    Script for generating list of spending by committees that haven't disclosed

---
+++ buckley/management/commands/non_disclosure_spending.py
@@ -0,0 +1,48 @@
+import csv
+
+from django.core.management.base import BaseCommand, CommandError
+
+from buckley.models import *
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        writer = csv.writer(open(r'non_disclosure_spending.csv', 'a'))
+
+        writer.writerow(['committee_id', 'committee', 'candidate_id', 'candidate',
+            'payee', 'expenditure_purpose', 'expenditure_date', 'expenditure_amount',
+            'support/oppose', 'election_type', 'amendment', 'race', 'filing_number', ])
+        
+        for committee in Committee.objects.exclude(tax_status='FECA Party'):
+            if committee.contribution_set.all():
+                continue
+
+            for expenditure in committee.expenditure_set.all():
+                if expenditure.electioneering_communication:
+                    row = [ 'EC',
+                            expenditure.committee.fec_id(),
+                            expenditure.committee,
+                            ', '.join(['%s (%s)' % (str(x), x.party) for x in expenditure.electioneering_candidates.all()]),
+                            expenditure.expenditure_purpose,
+                            expenditure.expenditure_date,
+                            expenditure.expenditure_amount,
+                            '',
+                            expenditure.election_type,
+                            expenditure.race,
+                            expenditure.filing_number,
+                            ]
+                else:
+                    row = [ 'IE',
+                            expenditure.committee.fec_id(),
+                            expenditure.committee,
+                            '%s (%s)' % (expenditure.candidate, expenditure.candidate.party),
+                            expenditure.expenditure_purpose,
+                            expenditure.expenditure_date,
+                            expenditure.expenditure_amount,
+                            expenditure.support_oppose,
+                            expenditure.election_type,
+                            expenditure.race,
+                            expenditure.filing_number,
+                            ]
+                writer.writerow(row)
+                print row
+
