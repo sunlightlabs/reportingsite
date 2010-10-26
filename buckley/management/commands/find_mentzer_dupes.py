753f6075b276aabcce92e027626d4691bfcc22d7
Aaron Bycoffe <bycoffe@gmail.com>
2010-10-26 06:56:55 -0400

    Script for finding Mentzer Media duplicates

---
+++ buckley/management/commands/find_mentzer_dupes.py
@@ -0,0 +1,23 @@
+from django.core.management.base import NoArgsCommand, BaseCommand, CommandError
+
+from buckley.models import *
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        dupes = {}
+        for expenditure in Expenditure.objects.filter(payee__name__icontains='Mentzer Media Services'):
+            e = Expenditure.objects.filter(candidate=expenditure.candidate,
+                                           committee=expenditure.committee,
+                                           expenditure_date=expenditure.expenditure_date,
+                                           expenditure_amount=expenditure.expenditure_amount).exclude(filing_number=expenditure.filing_number)
+            if e:
+                d = []
+                for i in e:
+                    if round(i.expenditure_amount) == round(expenditure.expenditure_amount):
+                        d.append(i)
+                if d:
+                    dupes[expenditure] = d
+
+        for k, v in dupes.items():
+            print k, v
