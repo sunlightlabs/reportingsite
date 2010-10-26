08e19dd8120df4239613396fe0c9f2cc0641ffa3
Aaron Bycoffe <bycoffe@gmail.com>
2010-10-26 16:06:16 -0400

    Change method for listing non-disclosers' expenditures

--- buckley/management/commands/co_disclosed_vs_nondisclosed.py
+++ buckley/management/commands/co_disclosed_vs_nondisclosed.py
@@ -13,14 +13,14 @@ class Command(BaseCommand):
         data = dict([(x, {'disclosers': 0, 'nondisclosers': 0}) for x in races])
         writer = csv.writer(sys.stdout)
         for race in races:
-            non_discloser_ids = Expenditure.objects.filter(race=race, committee__has_donors=False).order_by('committee').values_list('committee', flat=True)
+            non_discloser_ids = Expenditure.objects.filter(race=race, committee__has_donors=False).exclude(committee__tax_status='FECA Party').order_by('committee').values_list('committee', flat=True)
             non_disclosers = Committee.objects.filter(id__in=non_discloser_ids)
             for committee in non_disclosers:
                 amount = committee.expenditure_set.filter(race=race).aggregate(a=Sum('expenditure_amount'))['a']
                 writer.writerow([race, committee, amount, 'N'])
                 data[race]['nondisclosers'] += amount
 
-            discloser_ids = Expenditure.objects.filter(race=race, committee__has_donors=True).order_by('committee').values_list('committee', flat=True)
+            discloser_ids = Expenditure.objects.filter(race=race, committee__has_donors=True).exclude(committee__tax_status='FECA Party').order_by('committee').values_list('committee', flat=True)
             disclosers = Committee.objects.filter(id__in=discloser_ids)
             for committee in disclosers:
                 amount = committee.expenditure_set.filter(race=race).aggregate(a=Sum('expenditure_amount'))['a']
--- buckley/management/commands/non_disclosure_spending.py
+++ buckley/management/commands/non_disclosure_spending.py
@@ -4,6 +4,9 @@ from django.core.management.base import BaseCommand, CommandError
 
 from buckley.models import *
 
+def testing():
+    expenditures = Expenditure.objects.filter(committee__has_donors=False).exclude(committee__tax_status='FECA Party').order_by('committee', 'expenditure_date')
+
 class Command(BaseCommand):
     def handle(self, *args, **options):
         writer = csv.writer(open(r'non_disclosure_spending.csv', 'a'))
@@ -11,10 +14,44 @@ class Command(BaseCommand):
         writer.writerow(['committee_id', 'committee', 'candidate_id', 'candidate',
             'payee', 'expenditure_purpose', 'expenditure_date', 'expenditure_amount',
             'support/oppose', 'election_type', 'amendment', 'race', 'filing_number', ])
+
+        expenditures = Expenditure.objects.filter(committee__has_donors=False).exclude(committee__tax_status='FECA Party').order_by('committee', 'expenditure_date')
+        for expenditure in expenditures:
+            if expenditure.electioneering_communication:
+                row = [ 'EC',
+                        expenditure.committee.fec_id(),
+                        expenditure.committee,
+                        ', '.join(['%s (%s)' % (str(x), x.party) for x in expenditure.electioneering_candidates.all()]),
+                        expenditure.payee,
+                        expenditure.expenditure_purpose,
+                        expenditure.expenditure_date,
+                        expenditure.expenditure_amount,
+                        '',
+                        expenditure.election_type,
+                        expenditure.race,
+                        expenditure.filing_number,
+                        ]
+            else:
+                row = [ 'IE',
+                        expenditure.committee.fec_id(),
+                        expenditure.committee,
+                        '%s (%s)' % (expenditure.candidate, expenditure.candidate.party),
+                        expenditure.payee,
+                        expenditure.expenditure_purpose,
+                        expenditure.expenditure_date,
+                        expenditure.expenditure_amount,
+                        expenditure.support_oppose,
+                        expenditure.election_type,
+                        expenditure.race,
+                        expenditure.filing_number,
+                        ]
+            writer.writerow(row)
+            print row
+
+
         
-        for committee in Committee.objects.exclude(tax_status='FECA Party'):
-            if committee.contribution_set.all():
-                continue
+        return
+        for committee in Committee.objects.filter(has_donors=False).exclude(tax_status='FECA Party'):
 
             for expenditure in committee.expenditure_set.all():
                 if expenditure.electioneering_communication:
