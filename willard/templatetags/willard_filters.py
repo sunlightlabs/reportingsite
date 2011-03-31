6745904e0e37770e21777f810cf05038afc85f58
Aaron Bycoffe <bycoffe@gmail.com>
2011-03-31 11:43:33 -0400

    Show descriptions for fara filing types

--- willard/templatetags/willard_filters.py
+++ willard/templatetags/willard_filters.py
@@ -6,3 +6,20 @@ register = template.Library()
 def percentage(value, arg):
     """Get the percentage of value in arg."""
     return int(round((float(value)/float(arg))*100))
+
+@register.filter(name='form_definition')
+def form_definition(value):
+    """Return a description of the given type of form."""
+    definitions = {
+        'Registration Statement': 'Initial filing for primary registrant',
+        'Supplemental Statement': '6 month activities and financial information',
+        'Amendment': 'Corrections/additions to filings',
+        'Short-Form': 'Individual foreign agents',
+        'Exhibit A': 'Foreign principal',
+        'Exhibit AB': 'Foreign principal agreement',
+        'Exhibit B': 'Foreign principal agreement',
+        'Exhibit C': 'Articles of incorporation, bylaws, etc.',
+        'Exhibit D': 'Fundraising information',
+        'Conflict Provision': 'Related Statute; 18 U.S.C. 219',
+    }
+    return definitions.get(value, '')
