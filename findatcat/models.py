418b5b5e1be0f58c77f7668439cb8fca0f3bc99c
Jeremy Carbaugh <jcarbaugh@gmail.com>
2011-10-26 16:24:07 -0400

    add link model to findatcat

--- findatcat/models.py
+++ findatcat/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+import tagging
 
-# Create your models here.
+class Link(object):
+    title = models.CharField(max_length=128)
+    url = models.URLField(verify_exists=False)
+    description = models.TextField()
+    
+    class Meta:
+        ordering = ('title',)
+    
+    def __unicode__(self):
+        return self.title
+
+tagging.register(Link)
