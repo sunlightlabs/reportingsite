7b8ef52fe3f540c63e42eabd8d3da2c3d4428338
J Fenton <jfenton@js-macbook-pro.sunlightfoundation.net>
2012-02-22 15:07:53 -0500

    fix joshs file

--- rebuckley/views.py
+++ rebuckley/views.py
@@ -264,7 +264,7 @@ def states_csv(request):
 
     for c in states:
         
-        rows.append([c.state, c,dollarify(c.total_ind_exp),dollarify(c.total_pres_ind_exp), dollarify(c.total_house_ind_exp), dollarify(c.total_senate_ind_exp)])
+        rows.append([c.state, c,dollarify(c.total_ind_exp),dollarify(c.total_pres_ind_exp), dollarify(c.recent_pres_exp), dollarify(c.total_house_ind_exp), dollarify(c.total_senate_ind_exp)])
     return generic_csv_headless(file_name, fields, rows)  
 
 
