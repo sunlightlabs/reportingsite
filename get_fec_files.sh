55746e8d5ec74ca2cd815ea7f7bcd96ec0a0dfc5
J Fenton <jfenton@js-macbook-pro.sunlightfoundation.net>
2012-03-21 13:09:09 -0400

    update download shell script

--- get_fec_files.sh
+++ get_fec_files.sh
@@ -1,6 +1,6 @@
-datadir='re:buckley/data'
+datadir='outside_spending/data'
 
-for year in '10' '12'
+for year in '12'
 do
     echo "Getting files for: $year"
     
@@ -18,26 +18,10 @@ do
     #Independent expenditures
     curl -o $datadir/$year/IndependentExpenditure_$year.csv "http://www.fec.gov/data/IndependentExpenditure.do?format=csv&election_yr=20$year"
     
-    # Committee summary file -- only contains records from those that have filed a quarterly / monthly / annual report
-    curl -o $datadir/$year/CommitteeSummary_$year.csv "http://www.fec.gov/data/CommitteeSummary.do?format=csv&election_yr=20$year"
-    
-    # Indepedent expenditure file
-    curl -o $datadir/$year/CandidateSummary_$year.csv http://www.fec.gov/data/CandidateSummary.do?format=csv&election_yr=20$year
 
 done
 
-# The result should look like this:
-
-# $ ls  buckley/data/10/ buckley/data/12/
-# buckley/data/10/:
-# CandidateSummary_10.csv		cn10.zip
-# CommitteeSummary_10.csv		ec_exp_2010.csv
-# IndependentExpenditure_10.csv	foiacm.dta
-# cm10.zip			foiacn.dta
-# 
-# buckley/data/12/:
-# CandidateSummary_12.csv		cn12.zip
-# CommitteeSummary_12.csv		ec_exp_2012.csv
-# IndependentExpenditure_12.csv	foiacm.dta
-# cm12.zip			foiacn.dta
+# outside_spending/data/12/:
+# cn12.zip          foiacm.dta
+# cm12.zip          foiacn.dta
 
