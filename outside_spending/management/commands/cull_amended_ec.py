e0d659b2af3310f366538138b2838c95ae5f5401
J Fenton <jfenton@js-macbook-pro.sunlightfoundation.net>
2012-03-26 10:53:03 -0400

    more adds

---
+++ outside_spending/management/commands/cull_amended_ec.py
@@ -0,0 +1,54 @@
+from django.core.management.base import BaseCommand, CommandError
+from django.conf import settings
+
+from outside_spending.models import Electioneering_93, Electioneering_94
+
+def mark_as_superceded(filing_id):
+    
+    f93s = Electioneering_93.objects.filter(filing_number=filing_id)
+    for f in f93s:
+        f.superceded_by_amendment=True
+        f.save()
+    f94s = Electioneering_94.objects.filter(filing_number=filing_id)
+    
+    for f in f94s:
+        f.superceded_by_amendment=True
+        f.save()
+
+class Command(BaseCommand):
+    help = "Remove amended filings"
+    requires_model_validation = False
+    
+    def handle(self, *args, **options):
+        
+        # There's no programmatic way to remove amendments from this data. We're gonna have to ingest something else. 
+        """
+        mysql> select distinct filing_number, fec_id from outside_spending_Electioneering_93 where amnd_ind != 'N';
+        +---------------+-----------+
+        | filing_number | fec_id    |
+        +---------------+-----------+
+        |        766315 | C30001952 |
+        |        767351 | C30001655 |
+        |        767476 | C30001655 |
+        |        767479 | C30001655 |
+        +---------------+-----------+
+        4 rows in set (0.01 sec)
+        """
+        
+        # order: amended filing : original filing
+        amendment_table={
+        # crossroads: 
+        '767479':'764901',
+        '767476':'764753',
+        '767351':'764435',
+        # american conservative union
+        '766315':'764912'
+        }
+        
+        
+        for a in amendment_table:
+            print a, amendment_table[a]
+            
+        mark_as_superceded(amendment_table[a])    
+            
+        
