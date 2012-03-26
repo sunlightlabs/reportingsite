f92a254f20db582e791df0d7d51ed3dd814a4fbb
J Fenton <jfenton@js-macbook-pro.sunlightfoundation.net>
2012-03-26 15:09:56 -0400

    more csv fixes

--- outside_spending/views.py
+++ outside_spending/views.py
@@ -115,7 +115,7 @@ def state_contribs_csv(request, state):
     file_name = state + "_donors.csv"
 
     for c in contributions:
-        rows.append([ c.contrib_source(), c.superpac.name, c.fec_committeeid, c.contrib_org.replace('"',''), c.contrib_last.replace('"',''), c.contrib_first.replace('"',''), c.contrib_city.replace('"',''), c.contrib_state.replace('"',''), c.contrib_occupation.replace('"',''), c.contrib_employer.replace('"',''), c.contrib_amt, c.contrib_date, c.transaction_id, c.filing_number])
+        rows.append([ c.contrib_source(), c.committee.name, c.fec_committeeid, c.contrib_org.replace('"',''), c.contrib_last.replace('"',''), c.contrib_first.replace('"',''), c.contrib_city.replace('"',''), c.contrib_state.replace('"',''), c.contrib_occupation.replace('"',''), c.contrib_employer.replace('"',''), c.contrib_amt, c.contrib_date, c.transaction_id, c.filing_number])
     return generic_csv(file_name, fields, rows)        
 
 
@@ -130,7 +130,7 @@ def all_contribs_csv(request):
         if (c.committee):
             name = c.committee.name
 
-        rows.append([ c.contrib_source(), c.superpac.name, c.fec_committeeid, c.contrib_org.replace('"',''), c.contrib_last.replace('"',''), c.contrib_first.replace('"',''), c.contrib_city.replace('"',''), c.contrib_state.replace('"',''), c.contrib_occupation.replace('"',''), c.contrib_employer.replace('"',''), c.contrib_amt, c.contrib_date, c.transaction_id, c.filing_number])
+        rows.append([ c.contrib_source(), c.committee.name, c.fec_committeeid, c.contrib_org.replace('"',''), c.contrib_last.replace('"',''), c.contrib_first.replace('"',''), c.contrib_city.replace('"',''), c.contrib_state.replace('"',''), c.contrib_occupation.replace('"',''), c.contrib_employer.replace('"',''), c.contrib_amt, c.contrib_date, c.transaction_id, c.filing_number])
     return generic_csv(file_name, fields, rows)
     
     
