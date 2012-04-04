99ac365e6310b8120ec7434737e6fae6e5f01f77
J Fenton <jfenton@js-macbook-pro.sunlightfoundation.net>
2012-04-04 09:39:33 -0400

    make recent ies include those not from super pacs

--- outside_spending/views.py
+++ outside_spending/views.py
@@ -324,7 +324,7 @@ def state_detail(request, state_abbreviation):
 def ies(request):
     today = datetime.date.today()
     two_weeks_ago = today - datetime.timedelta(days=14)
-    ies = Expenditure.objects.select_related("committee", "candidate").filter(committee__is_superpac=True, superceded_by_amendment=False, expenditure_date__gte=two_weeks_ago).order_by('-expenditure_date')
+    ies = Expenditure.objects.select_related("committee", "candidate").filter(superceded_by_amendment=False, expenditure_date__gte=two_weeks_ago).order_by('-expenditure_date')
     explanatory_text= 'This page shows independent expenditures made in the last two weeks.'
     return render_to_response('outside_spending/expenditure_list.html',
                             {'ies':ies, 
