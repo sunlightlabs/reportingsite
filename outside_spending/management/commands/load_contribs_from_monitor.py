dc11a78dc8b70e4d4b415c9ea2539ff4b08bb407
Jacob Fenton <jfenton@js-macbook-pro.sunlightfoundation.net>
2012-04-20 16:33:19 -0400

    added spending adder

---
+++ outside_spending/management/commands/load_contribs_from_monitor.py
@@ -0,0 +1,64 @@
+import urllib
+import urllib2
+import re
+import time
+
+import socket
+
+from dateutil.parser import parse as dateparse
+
+
+from django.core.management.base import BaseCommand, CommandError
+from django.db.models import Sum
+
+from outside_spending.models import *
+
+from enter_f3x import enter_form
+
+# Give 'em 15 seconds to respond. 
+socket.setdefaulttimeout(15000)
+
+
+def download_with_headers(url):
+    headers = { 'User-Agent' : "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)" }    
+    req = urllib2.Request(url, None, headers)
+    page_read = urllib2.urlopen(req).read()
+    return page_read
+
+
+class Command(BaseCommand):
+    help = "Watches for new superpac F3X[N|A] (contrib) reports"
+    requires_model_validation = False
+
+
+    def handle(self, *args, **options):
+        
+        #all_superpacs = IEOnlyCommittee.objects.all().filter(total_presidential_indy_expenditures__gte=100)
+        #all_superpacs = IEOnlyCommittee.objects.all()
+        all_superpacs = Committee_Overlay.objects.filter(filing_frequency__iexact='M', is_superpac=True)
+        for sp in all_superpacs:
+            
+            try:
+                current_filing = F3X_Summary.objects.get(fec_id=sp.fec_id, coverage_to_date='2012-03-31')
+                print "Found Mar monthly filing from: %s" % (sp.name)
+                continue
+                            
+            except: 
+                print "Looking for filings from: %s" % (sp.name)
+            
+                # look for filings in the unprocess_filings model
+                f = None
+                try:
+                    f = unprocessed_filing.objects.get(form_type='F3XN', fec_id=sp.fec_id, coverage_to_date='2012-03-31')
+                except:
+                    continue
+                
+                
+                print "%s %s %s" % (f.committee_name, f.filing_number, f.coverage_to_date)
+                
+                dl_url = 'http://query.nictusa.com/dcdev/posted/%s.fec' % (f.filing_number)
+                print "\t===+Processing filing: %s, dl_url: %s" % (f.filing_number, dl_url)
+                this_page = download_with_headers(dl_url)
+                time.sleep(3)
+                enter_form(this_page, f.filing_number)
+            
