b26e50370d02ea75014f576a0acc2d7e7cc7009d
Jacob Fenton <jfenton@js-macbook-pro.sunlightfoundation.net>
2012-05-15 11:46:44 -0400

    add link to complete superpac list

--- outside_spending/views.py
+++ outside_spending/views.py
@@ -164,7 +164,7 @@ def all_contribs_csv(request):
     
 
 def all_superpacs(request):
-    explanatory_text = "This table shows all independent expenditure-only committees--better known as super PACS--that have raised at least $10,000 since the beginning of 2011. Click on the 'FEC filings' links to see the original filings on the Federal Election Commission's web site."
+    explanatory_text = "This table shows all independent expenditure-only committees--better known as super PACS--that have raised at least $10,000 since the beginning of 2011. Click on the 'FEC filings' links to see the original filings on the Federal Election Commission's web site. For the much longer list of <a href='/outside-spending/super-pacs/complete-list/'>all superpacs</a> click <a href='/outside-spending/super-pacs/complete-list/'>here</a>."
 
     superpacs = Committee_Overlay.objects.filter(total_contributions__gte=10000, is_superpac=True)
     total = superpacs.aggregate(total=Sum('total_indy_expenditures'))
