e27227c2196c550fd5c40310c47ac7487b1d951a
Jacob Fenton <jfenton@js-macbook-pro.sunlightfoundation.net>
2012-06-25 11:09:52 -0400

    additional logic in classifying superpac affiliations

--- outside_spending/scripts/classify_superpacs.py
+++ outside_spending/scripts/classify_superpacs.py
@@ -6,9 +6,10 @@ sys.path.append('/Users/jfenton/reporting/reportingsitenew/reportingsite')
 import settings
 setup_environ(settings)
 
-from outside_spending.models import Committee_Overlay, Filing_Header
+from outside_spending.models import Committee_Overlay, Filing_Header, Expenditure
 from outside_spending.form_parser import form_parser
 from outside_spending.filing import filing
+from django.db.models import Sum
 
 from dateutil.parser import parse as dateparse
 
@@ -38,9 +39,66 @@ for sp in superpacs:
         sp.save()
     elif (ie_support_reps > 0 and ie_support_dems > 0):
         print "*** %s is_ambiguous reps ; pro_dem: %s anti_dem: %s pro_rep: %s anti_rep: %s" % (sp.name, ie_support_dems, ie_oppose_dems, ie_support_reps, ie_oppose_reps)
-        sp.political_orientation = 'U'
-        sp.save()
-    
+        # Don't save, because we've probably set it by hand.
+        #sp.political_orientation = 'U'
+        #sp.save()
+        
+        
+    ## now look at primary spending and general election spending
+    elif (ie_support_reps == 0 and ie_support_dems == 0 and (ie_oppose_reps > 0 or ie_oppose_dems > 0 ) ):
+        
+        # flags
+        only_played_in_rep_primary = False
+        only_played_in_dem_primary = False        
+        opposed_dems_in_general = False
+        opposed_reps_in_general = False
+        
+        
+        #print "***Pure negative: %s" % sp.name
+        ies = Expenditure.objects.filter(committee=sp, superceded_by_amendment=False)
+        
+        for election_type in ('P', 'G'):
+            
+            all_ies = ies.filter(election_type=election_type)
+        
+            dem = all_ies.filter(candidate__party='DEM')   
+            total_dem_oppose = dem.filter(support_oppose='O').aggregate(total=Sum('expenditure_amount'))['total']
+
+            rep = all_ies.filter(candidate__party='REP')
+            total_rep_oppose = rep.filter(support_oppose='O').aggregate(total=Sum('expenditure_amount'))['total']
+
+            if not total_dem_oppose: 
+                total_dem_oppose = 0
+            if not total_rep_oppose:
+                total_rep_oppose = 0
+
+            # set flags
+            if (election_type == 'P'):
+                if (total_dem_oppose > 0 and total_rep_oppose == 0):
+                    only_played_in_dem_primary = True
+                elif (total_dem_oppose == 0 and total_rep_oppose > 0):
+                    only_played_in_rep_primary = True
+            if (election_type == 'G'):
+                if (total_dem_oppose > 0):
+                    opposed_dems_in_general = True
+                if (total_rep_oppose > 0):
+                    opposed_reps_in_general = True
+            
+            print "\t%s - For election type %s dem oppose: %s rep oppose: %s" % (sp.name, election_type, total_dem_oppose, total_rep_oppose)
+            
+        if (only_played_in_rep_primary and not opposed_reps_in_general):
+            print "%s Presumed Republican - only played in republican primary and didn't play in general" % (sp.name)
+            sp.political_orientation = 'R'
+            sp.save()
+            
+        if (only_played_in_dem_primary and not opposed_dems_in_general):
+            print "%s Presumed Democrat - only played in democratic primary and didn't play in general " % (sp.name)
+            sp.political_orientation = 'D'
+            sp.save()
+        if ( (only_played_in_dem_primary and opposed_dems_in_general) or (only_played_in_rep_primary and opposed_reps_in_general )):
+            print "!!!! confusing:%s" % (sp.name)
+            
+        
 print "totals D: %s R: %s" % (total_dem_contribs, total_rep_contribs)
 #    total_contribs = sp.total_contributions
     
