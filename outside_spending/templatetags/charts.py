c99b7f6194f96a162018ddad3b7cfe22eb3939c8
Jacob Fenton <jfenton@Js-MacBook-Pro.local>
2012-07-15 13:13:43 -0400

    add chart templatetags

--- outside_spending/templatetags/charts.py
+++ outside_spending/templatetags/charts.py
@@ -8,7 +8,7 @@ register = Library()
 
 # assumes hardcoded div of 
 @register.inclusion_tag('outside_spending/chart_templatetag.html')  
-def superpac_chart():
+def superpac_chart(div_to_return):
     
     superpac_ies = Expenditure.objects.filter(superceded_by_amendment=False, committee__is_superpac=True).extra(select={'year': 'EXTRACT(year FROM expenditure_date)','month': 'EXTRACT(month FROM expenditure_date)'}).values_list('year', 'month').order_by('year', 'month').annotate(Sum('expenditure_amount'))
     
@@ -23,8 +23,71 @@ def superpac_chart():
     # hack to only show contribs after the 20th of the month--which misses quarterly contribs, but... 
     m = datetime.timedelta(days=20)
     monthly_contrib_summary = summarize_monthly(monthly_contrib_data, today-m)
+    print monthly_contrib_summary
     
     return {
-    'monthly_ie_summary':monthly_ie_summary,
-    'monthly_contrib_summary':monthly_contrib_summary,
+    'has_series1':True,
+    'series1_data':monthly_ie_summary,
+    'series1_title':'MONTHLY INDEPENDENT EXPENDITURES',
+    'has_series2':True,
+    'series2_title':'MONTHLY CONTRIBUTIONS',
+    'series2_data':monthly_contrib_summary,
+    'return_div':div_to_return,
     }
+
+
+
+@register.inclusion_tag('outside_spending/chart_templatetag.html')  
+def noncommittee_spending(div_to_return):
+
+    noncommittee_ies = Expenditure.objects.filter(superceded_by_amendment=False,committee__ctype='I').extra(select={'year': 'EXTRACT(year FROM expenditure_date)','month': 'EXTRACT(month FROM expenditure_date)'}).values_list('year', 'month').order_by('year', 'month').annotate(Sum('expenditure_amount'))
+
+    today = datetime.datetime.today()
+
+    monthly_ie_summary = summarize_monthly(noncommittee_ies, today)
+
+    
+    return {
+    'has_series1':True,
+    'series1_data':monthly_ie_summary,
+    'series1_title':'NONCOMMITTEE INDEPENDENT EXPENDITURES',
+    'has_series2':False,
+    'return_div':div_to_return,
+    }
+    
+@register.inclusion_tag('outside_spending/chart_templatetag.html')  
+def nonparty_spending(div_to_return):
+
+    nonparty_ies = Expenditure.objects.filter(superceded_by_amendment=False,committee__ctype__in=('N', 'Q')).extra(select={'year': 'EXTRACT(year FROM expenditure_date)','month': 'EXTRACT(month FROM expenditure_date)'}).values_list('year', 'month').order_by('year', 'month').annotate(Sum('expenditure_amount'))
+
+    today = datetime.datetime.today()
+
+    monthly_ie_summary = summarize_monthly(nonparty_ies, today)
+
+
+    return {
+    'has_series1':True,
+    'series1_data':monthly_ie_summary,
+    'series1_title':'NON-PARTY INDEPENDENT EXPENDITURES',
+    'has_series2':False,
+    'return_div':div_to_return,
+    }    
+    
+    
+@register.inclusion_tag('outside_spending/chart_templatetag.html')  
+def party_spending(div_to_return):
+
+    party_ies = Expenditure.objects.filter(superceded_by_amendment=False,committee__ctype__in=('Y', 'Z')).extra(select={'year': 'EXTRACT(year FROM expenditure_date)','month': 'EXTRACT(month FROM expenditure_date)'}).values_list('year', 'month').order_by('year', 'month').annotate(Sum('expenditure_amount'))
+
+    today = datetime.datetime.today()
+
+    monthly_ie_summary = summarize_monthly(party_ies, today)
+
+
+    return {
+    'has_series1':True,
+    'series1_data':monthly_ie_summary,
+    'series1_title':'PARTY COMMITTEE INDEPENDENT EXPENDITURES',
+    'has_series2':False,
+    'return_div':div_to_return,
+    }    
--- outside_spending/urls.py
+++ outside_spending/urls.py
@@ -76,7 +76,7 @@ urlpatterns = patterns('',
     #url(r'^searchtest\/$', 'django.views.generic.simple.direct_to_template', {'template': 'mobile_test/searchtest.html'}),      
     url(r'^search\/$', 'outside_spending.views.search'),
     url(r'^more-resources\/$', 'outside_spending.views.more_resources'),
-    url(r'^charttest\/',direct_to_template, {'template': 'outside_spending/chart_test.html', 'extra_context': {'div_name':'superpac_chart'}}, ),
+    url(r'^charttest\/',direct_to_template, {'template': 'outside_spending/chart_test.html', 'extra_context': {'div_name':'superpac_chart', 'div_name_2':'noncommittees', 'div_name_3':'nonparty', 'div_name_4':'party'}}, ),
     url(r'$', 'outside_spending.views.all_superpacs')
     
     
