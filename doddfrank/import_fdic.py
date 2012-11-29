ec161c81bb57eae3ab10fb7114c1c1cd3f14e537
Drew Vogel <dvogel@sunlightfoundation.com>
2012-11-29 10:21:45 -0500

    Added org import to fdic importer.

--- doddfrank/import_fdic.py
+++ doddfrank/import_fdic.py
@@ -3,7 +3,8 @@ from argparse import ArgumentParser
 
 from doddfrank.importlib import (slurp_data, agency_or_die,
                                  reconcile_database, import_meetings,
-                                 import_attendees, prune_attendees, prune_organizations,
+                                 import_attendees, prune_attendees,
+                                 import_organizations, prune_organizations,
                                  ObjectCounts)
 from doddfrank.models import Agency, Attendee, Organization, Meeting
 
@@ -15,6 +16,7 @@ args = parser.parse_args()
 
 SCRAPER_MEETINGS_URL = 'https://api.scraperwiki.com/api/1.0/datastore/sqlite?format=jsondict&name=doddfrankfdic&query=select%20*%20from%20%60meetings%60'
 SCRAPER_ATTENDEES_URL = 'https://api.scraperwiki.com/api/1.0/datastore/sqlite?format=jsondict&name=doddfrankfdic&query=select%20*%20from%20%60attendees%60'
+SCRAPER_ORGANIZATIONS_URL = 'https://api.scraperwiki.com/api/1.0/datastore/sqlite?format=jsondict&name=doddfrankfdic&query=select%20*%20from%20%60organizations%60'
 
 
 FDIC = agency_or_die('FDIC')
@@ -58,6 +60,11 @@ def main():
     meetings = slurp_data(SCRAPER_MEETINGS_URL)
     import_meetings(meetings, meeting_keyfunc, meeting_copyfunc,
                     update_existing=args.update_existing)
+
+    print 'Importing organizations'
+    organizations = slurp_data(SCRAPER_ORGANIZATIONS_URL)
+    import_organizations(meetings, organizations, SharedKeys,
+                         'Org', meeting_keyfunc)
 
     print 'Importing attendees'
     attendees = slurp_data(SCRAPER_ATTENDEES_URL)
