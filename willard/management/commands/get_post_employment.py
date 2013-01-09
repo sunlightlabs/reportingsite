cf115e54dd1b250e6fcd6dfa2542c39e4b848fb4
Jacob Fenton <jfenton@sunlightfoundation.com>
2013-01-09 11:26:22 -0500

    add new post-employment tracker routines

---
+++ willard/management/commands/get_post_employment.py
@@ -0,0 +1,49 @@
+
+from django.core.management.base import BaseCommand
+from willard.management.commands import house_post_employment, senate_post_employment
+from optparse import make_option
+from datetime import datetime
+
+class Command(BaseCommand):
+
+    option_list = BaseCommand.option_list + (
+            make_option('-a', '--allyears',
+                action='store_true',
+                dest='allyears',
+                default=False,
+                help='Retrieve all records, not just the current year',
+            ),
+    )
+    
+    def handle(self, *args, **options):
+        
+        runallyears = options.get('allyears')
+        
+        today = datetime.now()
+        thisyear = today.year
+        thisyearstring = str(thisyear)
+        thismonthstring = str(today.month).zfill(2)
+        thisdaystring = str(today.day).zfill(2)
+        
+        start_year = thisyear
+        end_date = "%s/%s/%s" % (thismonthstring, thisdaystring, thisyearstring)
+        start_date = "01/01/%s" % (thisyearstring)
+        
+        if (runallyears):
+            start_year = 2008
+            # House started doing this in Nov. 07.
+            start_date = '11/01/2007'
+        
+        for year in range(start_year, thisyear+1):
+            print "retrieving senate records for year: %s" % (year)
+            #senate_post_employment.get_senate(year)
+        
+        print "retrieving house records for range %s - %s" % (start_date, end_date)
+        scraper = house_post_employment.postEmploymentScraper()
+        #scraper.scrape(start_date, end_date)
+            
+            
+        
+
+        
+        
---
+++ willard/management/commands/senate_post_employment.py
@@ -0,0 +1,54 @@
+"""
+http://www.senate.gov/legislative/termination_disclosure/2008/report2008.xml
+http://www.senate.gov/legislative/termination_disclosure/2009/report2009.xml
+"""
+import socket
+import urllib2
+from dateutil.parser import parse as dateparse
+import lxml.etree
+import lxml.html
+
+from django.db import IntegrityError
+from willard.models import PostEmploymentNotice
+
+socket.setdefaulttimeout(60*60*60)
+
+
+def get_senate(year):
+    if (year==2012):
+        url = 'http://www.senate.gov/legislative/termination_disclosure/report2012.xml'
+    else:
+        url = 'http://senate.gov/legislative/termination_disclosure/%s/report%s.xml' % (year, year)
+
+    print url
+    doc = lxml.etree.fromstring(urllib2.urlopen(url).read())
+    
+    print "Got doc %s" % (doc)
+
+    for employee in doc.xpath('previous_employee'):
+        employee = lxml.etree.fromstring(lxml.etree.tostring(employee))
+        data = {}
+        for key in ['first', 'middle', 'last', 
+                    'office_name', 'begin_date', 'end_date']:
+            try:
+                data[key] = employee.xpath('//%s' % key)[0].text
+            except IndexError:
+                data[key] = ''
+
+        data['body'] = 'Senate'
+        data['begin_date'] = dateparse(data['begin_date'])
+        try:
+            data['end_date'] = dateparse(data['end_date'])
+        except ValueError:
+            month, day, year = [int(x) for x in data['end_date'].split('/')]
+            data['end_date'] = datetime.date(year, month, day-1)
+
+        try:
+            employee = PostEmploymentNotice.objects.create(**data)
+            print "Added employee: %s" % (employee)
+        except IntegrityError:
+            pass
+
+        
+
+
