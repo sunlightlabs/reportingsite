84eb77aaf7165538911c45afdcbd8685783eadbe
Jacob Fenton <jfenton@sunlightfoundation.com>
2013-02-06 13:29:50 -0500

    fix syntax

--- reporting/templatetags/wptags.py
+++ reporting/templatetags/wptags.py
@@ -9,18 +9,18 @@ register = template.Library()
 # util methods
 
 def unix_time(dt):
-	epoch = datetime.datetime.utcfromtimestamp(0)
-	delta = dt - epoch
+    epoch = datetime.datetime.utcfromtimestamp(0)
+    delta = dt - epoch
 #	return delta.total_seconds()
 ## total_seconds() is python 2.7 forwards--instead use:
     return delta.days*86400+delta.seconds
 
 def unix_time_millis(dt):
-	return unix_time(dt) * 1000.0
+    return unix_time(dt) * 1000.0
 
 
 # filters
 
 @register.filter
 def rfc822(dt):
-	return formatdate(unix_time(dt))
+    return formatdate(unix_time(dt))
