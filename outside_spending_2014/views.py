4af30884f6355dc4c47bfd15d6054005189f1857
Jacob Fenton <jfenton@sunlightfoundation.com>
2013-04-12 00:41:58 -0400

    wording

--- outside_spending_2014/views.py
+++ outside_spending_2014/views.py
@@ -370,11 +370,11 @@ def committee_detail(request,cycle, committee_id):
     contributions = Contribution.objects.filter(fec_committeeid=committee_id, superceded_by_amendment=False, line_type__in=['SA11AI', 'SA11B', 'SA11C', 'SA12', 'SA15'])
     candidates_supported = Pac_Candidate.objects.filter(committee=committee)
     explanatory_text = "This table shows the overall total amount spent by this group supporting or opposing federal candidates in independent expenditures in the %s election cycle." % (cycle)
-    explanatory_text_details = 'This table shows the independent expenditures of $10,000 or more made by this group supporting or opposing federal candidates in the %s election cycle. To view a more detailed file of all such spending, <a href=\"%s\">click here</a>.' % (cycle, committee.superpachackcsv())
+    explanatory_text_details = 'This table shows the independent expenditures of $1,000 or more made by this group supporting or opposing federal candidates in the %s election cycle. To view a more detailed file of all such spending, <a href=\"%s\">click here</a>.' % (cycle, committee.superpachackcsv())
     explanatory_text_contribs = 'This table shows all contributions, related PAC transfers and operating expense offsets made to this group during the %s campaign cycle, as of %s. Operating expense offsets are marked with an asterisk (*). To view a more detailed file of this spending, which includes all receipt types, <a href=\"%s\">click here</a>.' % (cycle, committee.cash_on_hand_date,committee.superpachackdonorscsv())
     
 
-    display_expenditures = expenditures.filter(expenditure_amount__gte=10000)
+    display_expenditures = expenditures.filter(expenditure_amount__gte=1000)
     has_chart = False 
 
     return render_to_response('outside_spending_2014/committee_detail.html',
