9e7b8914bf393a8a6f58cbb9e01ae1a7a74519b9
Drew Vogel <dvogel@sunlightfoundation.com>
2013-06-07 10:58:51 -0400

    Added mgmt commands to dodd-frank for dumping csv files.

---
+++ doddfrank/management/commands/gen_attendee_spreadsheet.py
@@ -0,0 +1,50 @@
+from __future__ import print_function
+
+import sys
+
+from django.core.management.base import BaseCommand
+from optparse import make_option
+
+from unicodecsv import UnicodeCsvWriter
+from doddfrank.models import Meeting
+
+
+class Command(BaseCommand):
+    args = ''
+    help = 'Exports a CSV to stdout listing each meeting.'
+    option_list = BaseCommand.option_list + (
+        make_option('--agency', action='store', dest='agency', metavar='AGENCY',
+                    default=None, help='Limit the queries to this agency.'),
+    )
+
+    def handle(self, *args, **options):
+        print(u'Fetching meetings.',
+              file=sys.stderr)
+
+        meetings = Meeting.objects.all()
+        agency = options.get('agency')
+        if agency is not None:
+            meetings = meetings.filter(agency__initials=agency)
+
+        print(u'Writing CSV.',
+              file=sys.stderr)
+        writer = UnicodeCsvWriter(sys.stdout)
+
+        header_row = [u'Meeting Hash',
+                      u'Agency',
+                      u'Date',
+                      u'Communication Type',
+                      u'Attendee Org',
+                      u'Attendee Name',
+                     ]
+        writer.writerow(header_row)
+
+        for mtg in meetings:
+            for att in mtg.attendees.all():
+                writer.writerow([mtg.import_hash,
+                                 mtg.agency.slug,
+                                 mtg.date.strftime('%Y-%m-%d'),
+                                 mtg.communication_type,
+                                 att.org.name if att.org else u'',
+                                 att.name])
+
