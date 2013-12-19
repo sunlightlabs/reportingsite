5714096d56988d87840e71ebc48536b57f18e6be
Jacob Fenton <jfenton@sunlightfoundation.com>
2013-12-19 09:37:29 -0500

    change rss location as per fec data migration

--- fec_alerts/management/commands/scrape_rss_filings.py
+++ fec_alerts/management/commands/scrape_rss_filings.py
@@ -135,10 +135,16 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         
         new_filings = 0
-        # old URL
+        # old URL #1
         #rss_url = "http://query.nictusa.com/rss/newfilings.rss"
         # This was moved here, approximately 9/21/12
-        rss_url = "http://fecapps.nictusa.com/rss/generate?preDefinedFilingType=ALL"
+        
+        # old url #2. Migration out scheduled for end of 2013
+        # rss_url = "http://fecapps.nictusa.com/rss/generate?preDefinedFilingType=ALL"
+        
+        # Is this caching every 30 minutes or so? Should we break with querystring somehow? 
+        rss_url = "http://efilingapps.fec.gov/rss/generate?preDefinedFilingType=ALL"
+        
         my_logger.info('SCRAPE_DAILY_FILINGS - starting regular run')
         headers = {'User-Agent': USER_AGENT}   
         data = None       
