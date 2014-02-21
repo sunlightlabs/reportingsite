228016de84ac36330cbd9660423cd997e9edfd91
Jacob Fenton <jfenton@sunlightfoundation.com>
2014-02-21 09:53:32 -0500

    fec alert redirects

--- fec_alerts/urls.py
+++ fec_alerts/urls.py
@@ -1,21 +1,21 @@
 from django.conf.urls.defaults import patterns, url
+from django.views.generic.simple import redirect_to
 
 
 urlpatterns = patterns('',
 
-    url(r'^$', 'fec_alerts.views.recent_fec_filings'),
-    url(r'^superpacs\/?$', 'fec_alerts.views.recent_superpac_filings'),
-    url(r'^superpacs\/new-F3X\/?$', 'fec_alerts.views.recent_superpac_filings_f3x'),
-    url(r'^independent-expenditures\/?$', 'fec_alerts.views.recent_ie_filings'),    
-    url(r'^significant-committees\/?$', 'fec_alerts.views.significant_committees'), 
-    url(r'^significant-committees\/new-periodic\/?$', 'fec_alerts.views.significant_committees_new'), 
-    url(r'^48-hr-reports\/?$', 'fec_alerts.views.recent_fec_filings_48hr_contrib'), 
-    url(r'^significant-committees\/?$', 'fec_alerts.views.recent_fec_filings_significant'),
-    url(r'^significant-committees\/new-periodic\/?$', 'fec_alerts.views.recent_fec_filings_significant_new'),
+    url(r'^$', redirect_to, {'url':'http://realtime.influenceexplorer.com/newest-filings/'}),
+    url(r'^superpacs\/?$', redirect_to, {'url':'http://realtime.influenceexplorer.com/newest-filings/#?ordering=-filing_number&committee_class=UO&time_range=cycle'}),
+    
+    url(r'^superpacs\/new-F3X\/?$', redirect_to, {'url':'http://realtime.influenceexplorer.com/newest-filings/#?ordering=-filing_number&committee_class=UO&time_range=cycle&report_type=monthly'}),
+    url(r'^independent-expenditures\/?$', redirect_to, {'url':'http://realtime.influenceexplorer.com/newest-filings/#?ordering=-filing_number&time_range=cycle&report_type=ies'}),    
+    url(r'^significant-committees\/?$', redirect_to, {'url':'http://realtime.influenceexplorer.com/newest-filings/#?ordering=-filing_number&min_coh=1000000&time_range=cycle'}), 
+    url(r'^significant-committees\/new-periodic\/?$', redirect_to, {'url':'http://realtime.influenceexplorer.com/newest-filings/#?ordering=-filing_number&min_coh=1000000&time_range=cycle'}), 
+    url(r'^48-hr-reports\/?$', redirect_to, {'url':'http://realtime.influenceexplorer.com/newest-filings/#?ordering=-filing_number&time_range=cycle&report_type=F6'}), 
     #url(r'^48-hr-contrib-reports\/?$', 'fec_alerts.views.recent_fec_filings_48hr_contrib'),
-    url(r'^new-committees\/$', 'fec_alerts.views.new_committees'),
-    url(r'^new-superpacs\/$', 'fec_alerts.views.new_superpacs'),
-    url(r'^subscribe-to-alerts\/$', 'fec_alerts.views.subscribe_to_alerts'),
+    url(r'^new-committees\/$', redirect_to, {'url':'http://realtime.influenceexplorer.com/new-committees/'}),
+    url(r'^new-superpacs\/$', redirect_to, {'url':'http://realtime.influenceexplorer.com/new-committees/'}),
+    url(r'^subscribe-to-alerts\/$', redirect_to, {'url':'http://realtime.influenceexplorer.com/alerts/'}),
     url(r'^committee-search-html\/(?P<cycle>201[24])\/$', 'fec_alerts.views.committee_search_html'), 
     
     # url(r'^committee-search-json\/$', 'outside_spending.views.committee_search_json'),           
